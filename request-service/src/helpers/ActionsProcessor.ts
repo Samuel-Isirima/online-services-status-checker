e30f2c8a7c52f2eb0b4070c788878d418e4aad6d
Samuel Isirima <Samuel.isirima@yahoo.com>
2023-11-25 23:33:52 +0100

    Working on actions processor

---
+++ request-service/src/helpers/ActionsProcessor.ts
@@ -0,0 +1,6 @@
+import { IAction } from "../models/imported/Action";
+
+export const processActions = async (actions: IAction[]) => 
+{
+    //for each action, send a message to the notification service queue
+}
