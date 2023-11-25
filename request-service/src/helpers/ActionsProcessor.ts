97af3bf3cab2e736c7d4e09dc5c7f7f47d8c2cb6
Samuel Isirima <Samuel.isirima@yahoo.com>
2023-11-25 23:41:04 +0100

    Working on actions processor

--- request-service/src/helpers/ActionsProcessor.ts
+++ request-service/src/helpers/ActionsProcessor.ts
@@ -1,6 +1,34 @@
+import { type } from "os";
 import { IAction } from "../models/imported/Action";
+
+interface INotification {
+type: String;
+message: String;
+recipient: String;
+other_details: Object;
+}
 
 export const processActions = async (actions: IAction[]) => 
 {
     //for each action, send a message to the notification service queue
+    actions.forEach((action) => 
+    {
+        //Get action's notification configuration
+        const notificationConfiguration = action.notifications_config;
+        if(notificationConfiguration.email)
+        {
+            //Create notification object for email and send over to the notification service
+        }
+        if(notificationConfiguration.slack)
+        {
+            //create notification object for slack and send over to the notification service
+        }
+        if(notificationConfiguration.sms)
+        {
+            //create notification object for sms and send over to the notification service
+        }
+        //Send a message to the notification service queue
+        //The message should contain the action id and the interested response id
+        //The notification service will then get the action and the interested response and then send a notification to the user
+    });
 }
