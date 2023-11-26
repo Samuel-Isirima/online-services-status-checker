290f40fbec0aef92eb19a8a0ab27bae21b4ce0c6
Samuel Isirima <Samuel.isirima@yahoo.com>
2023-11-26 21:42:36 +0100

    Send notification to queue

--- request-service/src/helpers/ActionsProcessor.ts
+++ request-service/src/helpers/ActionsProcessor.ts
@@ -1,6 +1,7 @@
 import { type } from "os";
 import { IAction } from "../models/imported/Action";
 import InterestedResponse from "../models/imported/InterestedResponse";
+const amqp = require('amqplib')
 
 interface INotification {
 type: String;
@@ -11,17 +12,20 @@ other_details: Object;
 
 export const processActions = async (actions: IAction[]) => 
 {
+    //Create connection to the notification service queue
+
+
     //for each action, send a message to the notification service queue
     actions.forEach(async (action) => 
     {
         //Get action's notification configuration
         const notificationConfiguration = action.notifications_config;
+                    //Get user's email address
+        const user = await User.findOne({ _id: action.user_id });
+        
         if(notificationConfiguration.email)
         {
             //Create notification object for email and send over to the notification service
-
-            //Get user's email address
-            const user = await User.findOne({ _id: action.user_id });
             const email = user.email;
 
             const notification: INotification = 
@@ -31,17 +35,57 @@ export const processActions = async (actions: IAction[]) =>
                 recipient: email,
                 other_details: {}
             }
+
+            sendNotificationObjectToQueue(notification);
+
         }
         if(notificationConfiguration.slack)
         {
+            const slack = user.slack;
+
+            const notification: INotification = 
+            {
+                type: "slack",
+                message: action.custom_message,
+                recipient: slack,
+                other_details: {}
+            }
             //create notification object for slack and send over to the notification service
         }
         if(notificationConfiguration.sms)
         {
+            const sms = user.sms;
+
+            const notification: INotification = 
+            {
+                type: "sms",
+                message: action.custom_message,
+                recipient: sms,
+                other_details: {}
+            }
             //create notification object for sms and send over to the notification service
         }
         //Send a message to the notification service queue
         //The message should contain the action id and the interested response id
         //The notification service will then get the action and the interested response and then send a notification to the user
     });
+}
+
+
+const sendNotificationObjectToQueue = async (notification: INotification) =>
+{
+    const queue = "notification_service_queue";
+
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+    
+        await channel.assertQueue(queue, { durable: false });
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(notification)));
+        await channel.close();
+      } 
+      catch (err) 
+      {
+        console.warn(err);
+      }
 }
