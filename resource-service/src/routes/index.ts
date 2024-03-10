e9cd8aba60e1654873ec9f336b3d3d1e6f4bba74
samuel-isirima <samuel.isirima@yahoo.com>
2024-03-11 00:42:15 +0100

    Working on request CRU operations now

--- resource-service/src/routes/index.ts
+++ resource-service/src/routes/index.ts
@@ -1,11 +1,13 @@
 import e, { Router } from 'express'
 import collectionRouter from './collection';
 import resourceRouter from './resource';
+import requestRouter from './request';
 
 const router = Router()
 
 router.use('/resource/collection', collectionRouter)
 router.use('/resource', resourceRouter)
+router.use('/resource/request', requestRouter)
 
 
 export default router;
---
+++ resource-service/src/routes/request.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response, NextFunction} from 'express'
+import { Auth } from '../middleware/Auth';
+const dotenv = require('dotenv');
+dotenv.config();
+const ResourceController = require('../controllers/ResourceController');
+
+const resourceRouter: Router = Router()
+
+
+
+resourceRouter.post('/create', Auth, ResourceController.create)
+resourceRouter.get('/', Auth, ResourceController.index)
+resourceRouter.get('/:identifier', Auth, ResourceController.get)
+resourceRouter.post('/:identifier/update', Auth, ResourceController.update)
+
+export default resourceRouter;
